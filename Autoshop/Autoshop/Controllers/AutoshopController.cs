271042f9aed73557b98564ac0568bca387d08294
Pin131GasanowO <cherep.monolit@yandex.ru>
2017-02-04 18:55:50 +0300

    Собран проект

---
+++ Autoshop/Autoshop/Controllers/AutoshopController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Autoshop.Controllers
+{
+    public class AutoshopController : Controller
+    {
+        // GET: Autoshop
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Autoshop/Autoshop/Models/Autoshop/AutoshopGenerator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Autoshop.Models.Autoshop
+{
+    public class AutoshopGenerator
+    {
+    }
+}
