77fe2de7228ed1ec181dac0dc26b3818ec4914fe
Pin131GasanowO <cherep.monolit@yandex.ru>
2017-03-06 21:31:20 +0300

    Ошибки

---
+++ ComputerShop/ComputerShop/Models/Catalog/Article.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ComputerShop.Models.Common;
+
+namespace ComputerShop.Models.Catalog
+{
+    public class Article : KeyGen
+    {
+        public string Label { get; set; }
+        public decimal Price { get; set; }
+        public decimal Balance { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+    }
+}
---
+++ ComputerShop/ComputerShop/Models/Common/KeyGen.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComputerShop.Models.Common
+{
+    public class KeyGen<TKey>
+    {
+        public TKey Id { get; set; }
+    }
+    public class KeyGen : KeyGen<int?> { }
+}
